c9da6ecc90d5c5ca28693205824a9aaf28dc3b2f
Overrideveloper <bansowisdom@gmail.com>
2017-06-17 13:04:21 +0100

    Added Leave Controller, Added Leave Index View

---
+++ BizzDesk-Leap-Client/Areas/HRAdmin/Controllers/LeaveController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BizzDesk_Leap_Client.Areas.HRAdmin.Models;
+using BizzDesk_Leap_Client.Areas.HRAdmin.ViewModels;
+using PagedList;
+
+namespace BizzDesk_Leap_Client.Areas.HRAdmin.Controllers
+{
+    public class LeaveController : Controller
+    {
+        LeaveClient lc;
+        public LeaveController()
+        {
+            lc = new LeaveClient();
+        }
+
+        //
+        // GET: /HRAdmin/Leave/
+        public ActionResult Index(int? page)
+        {
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            ViewBag.LeaveList = lc.findAll().ToPagedList(pageNumber, pageSize);
+            return View();
+        }
+	}
+}
