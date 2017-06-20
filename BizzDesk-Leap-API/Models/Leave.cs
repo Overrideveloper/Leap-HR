bacff1bb16989a050936c710a4a8de6d8bb5cf50
Overrideveloper <bansowisdom@gmail.com>
2017-06-20 15:12:50 +0100

    Major Modifications

---
+++ BizzDesk-Leap-API/Models/Leave.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using BizzDesk_Leap_API.Enums;
+
+namespace BizzDesk_Leap_API.Models
+{
+    public partial class Leave
+    {
+        [Key]
+        [Required]
+        public int ID { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public LeaveType LeaveType { get; set; }
+    }
+}
