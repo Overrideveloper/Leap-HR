2ac787dfdd955a15309f0d0e217a8118615fde03
Overrideveloper <bansowisdom@gmail.com>
2017-06-21 10:46:17 +0100

    Added Dashboard Controller, DashboardViewModel and Dashboard Index

---
+++ BizzDesk-Leap-Client/Areas/HRAdmin/Controllers/DashboardController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BizzDesk_Leap_Client.Areas.HRAdmin.Models;
+
+namespace BizzDesk_Leap_Client.Areas.HRAdmin.Controllers
+{
+    public class DashboardController : Controller
+    {
+        DepartmentClient dc;
+        RankClient rc;
+        EmployeeClient ec;
+        LeaveClient lc;
+
+        public DashboardController()
+        {
+            dc = new DepartmentClient();
+            rc = new RankClient();
+            ec = new EmployeeClient();
+            lc = new LeaveClient();
+        }
+
+        //
+        // GET: /HRAdmin/Dashboard/
+        public ActionResult Index()
+        {
+            ViewBag.DepartmentCount = dc.findAll().ToArray().Length;
+            ViewBag.RankCount = rc.findAll().ToArray().Length;
+            ViewBag.EmployeeCount = ec.findAll().ToArray().Length;
+            ViewBag.LeaveCount = lc.findAll().ToArray().Length;
+            return View();
+        }
+	}
+}
---
+++ BizzDesk-Leap-Client/Areas/HRAdmin/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BizzDesk_Leap_Client.Areas.HRAdmin.Models;
+
+namespace BizzDesk_Leap_Client.Areas.HRAdmin.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public DepartmentClient DepartmentClient
+        {
+            get;
+            set;
+        }
+
+        public RankClient RankClient
+        {
+            get;
+            set;
+        }
+
+        public EmployeeClient EmployeeClient
+        {
+            get;
+            set;
+        }
+
+        public LeaveClient LeaveClient
+        {
+            get;
+            set;
+        }
+    }
+}
