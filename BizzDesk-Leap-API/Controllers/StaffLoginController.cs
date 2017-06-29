f46493620f9a5d3569f724d84fc16589b9a6716b
Overrideveloper <bansowisdom@gmail.com>
2017-06-29 21:18:11 +0100

    Added staff login controller

---
+++ BizzDesk-Leap-API/Controllers/StaffLoginController.cs
@@ -0,0 +1,37 @@
+﻿using BizzDesk_Leap_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using BizzDesk_Leap_API.DAL;
+
+namespace BizzDesk_Leap_API.Controllers
+{
+    public class StaffLoginController : ApiController
+    {
+
+        private LeapDB db;
+        public StaffLoginController()
+        {
+            db = new LeapDB();       
+        }
+
+        // POST api/Employee
+        [ResponseType(typeof(Employee))]
+        public IHttpActionResult PostEmployee(Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Employee.Add(employee);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = employee.ID }, employee);
+        }
+    }
+}
