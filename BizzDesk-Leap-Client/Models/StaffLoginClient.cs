070e96352e0c3662ea743544f27873349610f15a
Overrideveloper <bansowisdom@gmail.com>
2017-06-30 12:24:13 +0100

    Added StaffLoginClient

---
+++ BizzDesk-Leap-Client/Models/StaffLoginClient.cs
@@ -0,0 +1,32 @@
+﻿using BizzDesk_Leap_Client.Areas.HRAdmin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+
+namespace BizzDesk_Leap_Client.Models
+{
+    public class StaffLoginClient
+    {
+        private string BASE_URL = "http://localhost:61040/api/";
+
+        public bool Login(Employee employee)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri(BASE_URL);
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = client.PostAsJsonAsync("employee", employee).Result;
+                return response.IsSuccessStatusCode;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
