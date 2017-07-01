ede57c1289a33bdc8a872b726a7defbe5b51c916
Overrideveloper <bansowisdom@gmail.com>
2017-07-01 22:29:36 +0100

    Added Dashboard Controller

---
+++ BizzDesk-Leap-Client/Areas/Employee/Controllers/DashboardController.cs
@@ -0,0 +1,27 @@
+﻿using BizzDesk_Leap_Client.Areas.HRAdmin.Models;
+using BizzDesk_Leap_Client.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BizzDesk_Leap_Client.Areas.Employee.Controllers
+{
+    public class DashboardController : BaseController
+    {
+        EmployeeClient ec;
+        public DashboardController()
+        {
+            ec = new EmployeeClient();
+        }
+
+        //
+        // GET: /Employee/Dashboard/
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+	}
+}
