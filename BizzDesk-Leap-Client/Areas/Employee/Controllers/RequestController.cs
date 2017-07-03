ae6fabc235112619cab821010b97326e88d1e840
Overrideveloper <bansowisdom@gmail.com>
2017-07-03 15:46:05 +0100

    Request Module Work

---
+++ BizzDesk-Leap-Client/Areas/Employee/Controllers/RequestController.cs
@@ -0,0 +1,36 @@
+﻿using BizzDesk_Leap_Client.Areas.Employee.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using BizzDesk_Leap_Client.Areas.Employee.ViewModels;
+using BizzDesk_Leap_Client.Controllers;
+
+namespace BizzDesk_Leap_Client.Areas.Employee.Controllers
+{
+    public class RequestController : BaseController
+    {
+        RequestClient rc;
+        RequestViewModel rvm;
+
+        public RequestController()
+        {
+            rc = new RequestClient();
+            rvm = new RequestViewModel();
+        }
+
+        // GET: /Employee/Request/
+        public ActionResult Index(int? page)
+        {
+            var id = Convert.ToInt32(Session["ID"]);
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            ViewBag.RequestsList = rc.findAll().Where(s => s.EmployeeID == id).ToPagedList(pageNumber, pageSize);
+            return View();
+        }
+
+
+	}
+}
--- BizzDesk-Leap-Client/Controllers/StaffLoginController.cs
+++ BizzDesk-Leap-Client/Controllers/StaffLoginController.cs
@@ -36,8 +36,6 @@ namespace BizzDesk_Leap_Client.Controllers
                 Session["FName"] = usr.FirstName.ToString();
                 Session["LName"] = usr.LastName.ToString();
                 Session["UserID"] = usr.EmployeeID.ToString();
-                Session["Dept"] = usr.Department.Title.ToString();
-                Session["Rank"] = usr.Rank.Title.ToString();
                 return RedirectToAction("Index", "Dashboard", new { Area = "Employee" });
             }
             else
