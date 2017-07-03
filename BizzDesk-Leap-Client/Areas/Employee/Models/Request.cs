3df71593eab4fb1f71ec614f567ece3a6e968568
Overrideveloper <bansowisdom@gmail.com>
2017-07-03 14:37:31 +0100

    Added Request model in Client

---
+++ BizzDesk-Leap-Client/Areas/Employee/Models/Request.cs
@@ -0,0 +1,40 @@
+﻿using BizzDesk_Leap_Client.Areas.Employee.Enums;
+using BizzDesk_Leap_Client.Areas.HRAdmin.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BizzDesk_Leap_Client.Areas.Employee.Models
+{
+    public class Request
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Select start date")]
+        public DateTime StartDate { get; set; }
+
+        [Required(ErrorMessage = "Select end date")]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        public DateTime RequestDate { get; set; }
+
+        public Status Status { get; set; }
+
+        //Entity Relationships
+
+        [ForeignKey("Employee")]
+        public int EmployeeID { get; set; }
+
+        public virtual BizzDesk_Leap_Client.Areas.HRAdmin.Models.Employee Employee { get; set; }
+
+        [Required]
+        public int LeaveID { get; set; }
+
+        public virtual Leave Leave { get; set; }
+    }
+}
