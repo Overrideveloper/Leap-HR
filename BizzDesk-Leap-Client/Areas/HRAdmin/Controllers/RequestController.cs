3ac5b6642ac8cc71ac1784a97d54f318e8aa0071
Overrideveloper <bansowisdom@gmail.com>
2017-07-06 13:22:07 +0100

    Added request client controller

---
+++ BizzDesk-Leap-Client/Areas/HRAdmin/Controllers/RequestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BizzDesk_Leap_Client.Areas.HRAdmin.Controllers
+{
+    public class RequestController : Controller
+    {
+        //
+        // GET: /HRAdmin/Request/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
