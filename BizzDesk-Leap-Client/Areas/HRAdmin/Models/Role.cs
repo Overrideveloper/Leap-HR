b0e4ef40f4917c5d02cdefc49f27e591178275de
Overrideveloper <bansowisdom@gmail.com>
2017-07-07 14:39:02 +0100

    Modified Role model in client

--- BizzDesk-Leap-Client/Areas/HRAdmin/Models/Role.cs
+++ BizzDesk-Leap-Client/Areas/HRAdmin/Models/Role.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,24 @@ namespace BizzDesk_Leap_Client.Areas.HRAdmin.Models
 {
     public class Role
     {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public bool CanManageDepartments { get; set; }
+
+        public bool CanManageRanks { get; set; }
+
+        public bool CanManageEmployees { get; set; }
+
+        public bool CanManageLeaves { get; set; }
+
+        public bool CanManageRequests { get; set; }
+
+        public bool CanManageRoles { get; set; }
+
+        public bool CanManageUsers { get; set; }
     }
 }
