708e1f5e48ecf6e528c6c3fc0839e55439f88efa
Overrideveloper <bansowisdom@gmail.com>
2017-07-07 14:37:51 +0100

    Added Role model in Client

---
+++ BizzDesk-Leap-Client/Areas/HRAdmin/Models/Role.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BizzDesk_Leap_Client.Areas.HRAdmin.Models
+{
+    public class Role
+    {
+    }
+}
