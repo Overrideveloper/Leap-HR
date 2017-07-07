fe396667eef74dd56dd3fdb48fedbf99be86cdb9
Overrideveloper <bansowisdom@gmail.com>
2017-07-07 14:56:28 +0100

    Modified Role Client

--- BizzDesk-Leap-Client/Areas/HRAdmin/Models/RoleClient.cs
+++ BizzDesk-Leap-Client/Areas/HRAdmin/Models/RoleClient.cs
@@ -1,11 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web;
 
 namespace BizzDesk_Leap_Client.Areas.HRAdmin.Models
 {
     public class RoleClient
     {
+        private string BASE_URL = "http://localhost:61040/api/";
+
+        public IEnumerable<Role> findAll()
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri(BASE_URL);
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = client.GetAsync("role/readall").Result;
+
+                if (response.IsSuccessStatusCode)
+                    return response.Content.ReadAsAsync<IEnumerable<Role>>().Result;
+                return null;
+            }
+            catch
+            {
+                return null;
+            }
+        }
     }
 }
