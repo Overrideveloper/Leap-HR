6e5890fe30e6a784865f807b490a05437f4c3b9a
Overrideveloper <bansowisdom@gmail.com>
2017-07-07 03:18:14 +0100

    Added Role Model

---
+++ BizzDesk-Leap-API/Models/Role.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BizzDesk_Leap_API.Models
+{
+    public class Role
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public bool CanManageDepartments { get; set; }
+
+        public bool CanManageRanks { get; set; }
+
+        public bool CanManageEmployees { get; set; }
+
+        public bool CanManageLeaves { get; set; }
+
+        public bool CanManageRequests { get; set; }
+
+        public bool CanManageRoles { get; set; }
+
+        public bool CanManageUsers { get; set; }
+    }
+}
