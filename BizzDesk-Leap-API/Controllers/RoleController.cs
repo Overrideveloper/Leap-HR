3ff8c07f1c6d0fc7a20f510bdf7f07d6516dc0dd
Overrideveloper <bansowisdom@gmail.com>
2017-07-07 03:39:39 +0100

    Added role api controller

---
+++ BizzDesk-Leap-API/Controllers/RoleController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using BizzDesk_Leap_API.Models;
+using BizzDesk_Leap_API.DAL;
+
+namespace BizzDesk_Leap_API.Controllers
+{
+    public class RoleController : ApiController
+    {
+        private LeapDB db = new LeapDB();
+
+        // GET api/Role
+        public IQueryable<Role> GetRole()
+        {
+            return db.Role;
+        }
+
+        // GET api/Role/5
+        [ResponseType(typeof(Role))]
+        public IHttpActionResult GetRole(int id)
+        {
+            Role role = db.Role.Find(id);
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(role);
+        }
+
+        // PUT api/Role/5
+        public IHttpActionResult PutRole(int id, Role role)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != role.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(role).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RoleExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST api/Role
+        [ResponseType(typeof(Role))]
+        public IHttpActionResult PostRole(Role role)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Role.Add(role);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = role.ID }, role);
+        }
+
+        // DELETE api/Role/5
+        [ResponseType(typeof(Role))]
+        public IHttpActionResult DeleteRole(int id)
+        {
+            Role role = db.Role.Find(id);
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            db.Role.Remove(role);
+            db.SaveChanges();
+
+            return Ok(role);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool RoleExists(int id)
+        {
+            return db.Role.Count(e => e.ID == id) > 0;
+        }
+    }
+}
