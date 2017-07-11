985c5b692704d463fa3d9dac0942899af9e9fc77
Overrideveloper <bansowisdom@gmail.com>
2017-07-11 14:30:36 +0100

    Installed AspNet.WebApi.HelpPages

---
+++ BizzDesk-Leap-API/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace BizzDesk_Leap_API.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
