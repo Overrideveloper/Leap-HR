cd063a36b0258d4ebf8ff79dabe4e9eee9f7b57b
Overrideveloper <bansowisdom@gmail.com>
2017-07-14 16:33:25 +0100

    Added datatable script

---
+++ BizzDesk-Leap-Client/Scripts/datatable.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    $('#grid').DataTable({
+        columnDefs: [
+            {
+                targets: [0, 1, 2],
+                className: 'dataTable'
+            }
+        ]
+    });
+});
