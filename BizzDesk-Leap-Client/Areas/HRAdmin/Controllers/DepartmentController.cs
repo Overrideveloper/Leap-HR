37b19b398001210a0dadd703c1983d8fa17d69c3
Overrideveloper <bansowisdom@gmail.com>
2017-07-14 17:54:58 +0100

    Modified department client controller

--- BizzDesk-Leap-Client/Areas/HRAdmin/Controllers/DepartmentController.cs
+++ BizzDesk-Leap-Client/Areas/HRAdmin/Controllers/DepartmentController.cs
@@ -91,17 +91,5 @@ namespace BizzDesk_Leap_Client.Areas.HRAdmin.Controllers
             return RedirectToAction("Index");
         }
 
-        [HttpGet]
-        public ActionResult Search()
-        {
-            return View("Search", new DepartmentViewModel());
-        }
-
-        [HttpPost]
-        public ActionResult Search(DepartmentViewModel dvm)
-        {
-            ViewBag.Result = dc.Search(dvm.Department.Title).Result;
-            return View("Search");
-        }
 	}
 }
