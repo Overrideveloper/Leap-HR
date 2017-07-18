f3daa3764c576a18d449ae3642f658893ff5c993
Overrideveloper <bansowisdom@gmail.com>
2017-07-18 23:02:46 +0100

    Added ApplicationOAuthProvider

---
+++ BizzDesk-Leap-API/App_Start/Startup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using BizzDesk_Leap_API.Security;
+using Microsoft.Owin.Security.OAuth;
+
+
+[assembly: OwinStartup(typeof(BizzDesk_Leap_API.App_Start.Startup))]
+namespace BizzDesk_Leap_API.App_Start
+{
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            app.CreatePerOwinContext(ApplicationUserDBContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+
+            var oAuthOptions = new OAuthAuthorizationServerOptions
+            {
+                TokenEndpointPath = new PathString("/api/token"),
+                Provider = new ApplicationOAuthProvider(),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                AllowInsecureHttp = true
+            };
+
+            //enable use of beare tokens to authenticate users
+            app.UseOAuthBearerTokens(oAuthOptions);
+        }
+    }
+}
