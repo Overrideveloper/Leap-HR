479bf6a368e898a25ad822390a80ba86072645c6
Overrideveloper <bansowisdom@gmail.com>
2017-07-18 15:49:15 +0100

    Created an IdentityUser class

---
+++ BizzDesk-Leap-API/Security/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BizzDesk_Leap_API.Security
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
