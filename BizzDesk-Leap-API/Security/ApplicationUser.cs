c34bfe07303eb04b249f36b963dd768326d64df1
Overrideveloper <bansowisdom@gmail.com>
2017-07-18 23:10:17 +0100

    Modified ApplicationUser.cs

--- BizzDesk-Leap-API/Security/ApplicationUser.cs
+++ BizzDesk-Leap-API/Security/ApplicationUser.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace BizzDesk_Leap_API.Security
 {
     public class ApplicationUser : IdentityUser
     {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
+        {
+            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+            return userIdentity;
+        }
     }
 }
