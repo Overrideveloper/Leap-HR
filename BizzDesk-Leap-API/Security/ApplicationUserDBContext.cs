6163faf24e058c73da8c086fbace88944a2a2476
Overrideveloper <bansowisdom@gmail.com>
2017-07-18 15:53:54 +0100

    Added Application User Database Context

---
+++ BizzDesk-Leap-API/Security/ApplicationUserDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BizzDesk_Leap_API.Security
+{
+    public class ApplicationUserDBContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationUserDBContext() : base("Leap") { }
+
+        public static ApplicationUserDBContext Create()
+        {
+            return new ApplicationUserDBContext();
+        }
+    }
+}
