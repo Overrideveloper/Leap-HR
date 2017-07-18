ce3117f6446c13a928af9cd01e9eb1348851e553
Overrideveloper <bansowisdom@gmail.com>
2017-07-18 23:14:26 +0100

    Added Register Model

---
+++ BizzDesk-Leap-API/Security/Register.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BizzDesk_Leap_API.Security
+{
+    public class Register
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long", MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The passwords don't match")]
+        public string  ConfirmPassword { get; set; }
+    }
+}
