272b003ce2d8a6a0a8e4cec74d5f501a74c75b28
Overrideveloper <bansowisdom@gmail.com>
2017-07-19 13:23:07 +0100

    Added Register Model to Client

---
+++ BizzDesk-Leap-Client/Areas/HRAdmin/Security/RegisterModel.cs
@@ -0,0 +1,27 @@
+﻿using BizzDesk_Leap_Client.Areas.HRAdmin.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BizzDesk_Leap_Client.Areas.HRAdmin.Security
+{
+    public class RegisterModel
+    {
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [ForeignKey("Role")]
+        public int RoleID { get; set; }
+
+        public virtual Role Role { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
