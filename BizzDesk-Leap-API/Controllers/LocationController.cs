034360b787ce36dfc5570c69eb4df27c015c01db
Overrideveloper <bansowisdom@gmail.com>
2017-07-26 15:38:43 +0100

    Added Location Controller

---
+++ BizzDesk-Leap-API/Controllers/LocationController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using BizzDesk_Leap_API.Models;
+using BizzDesk_Leap_API.DAL;
+
+namespace BizzDesk_Leap_API.Controllers
+{
+    public class LocationController : ApiController
+    {
+        private LeapDB db = new LeapDB();
+
+        // GET api/Location
+        public IQueryable<Location> GetLocation()
+        {
+            return db.Location;
+        }
+
+        // GET api/Location/5
+        [ResponseType(typeof(Location))]
+        public IHttpActionResult GetLocation(int id)
+        {
+            Location location = db.Location.Find(id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(location);
+        }
+
+        // PUT api/Location/5
+        public IHttpActionResult PutLocation(int id, Location location)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != location.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(location).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LocationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST api/Location
+        [ResponseType(typeof(Location))]
+        public IHttpActionResult PostLocation(Location location)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Location.Add(location);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = location.ID }, location);
+        }
+
+        // DELETE api/Location/5
+        [ResponseType(typeof(Location))]
+        public IHttpActionResult DeleteLocation(int id)
+        {
+            Location location = db.Location.Find(id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+
+            db.Location.Remove(location);
+            db.SaveChanges();
+
+            return Ok(location);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool LocationExists(int id)
+        {
+            return db.Location.Count(e => e.ID == id) > 0;
+        }
+    }
+}
