e1d3cec6cdcc185029a8fcb385c39c1193a4d545
Overrideveloper <bansowisdom@gmail.com>
2017-07-27 12:01:45 +0100

    Added Leave Constraint data object

---
+++ BizzDesk-Leap-API/Models/LeaveConstraint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+
+namespace BizzDesk_Leap_API.Models
+{
+    public class LeaveConstraint
+    {
+        [DisplayName("Gender")]
+        public string? GenderConstraint { get; set; }
+
+        [DisplayName("Department")]
+        public int? DepartmentID { get; set; }
+        public virtual Department DepartmentConstraint { get; set; }
+
+        [DisplayName("Rank")]
+        public int? RankID { get; set; }
+        public virtual Rank RankConstraint { get; set; }
+    }
+}
