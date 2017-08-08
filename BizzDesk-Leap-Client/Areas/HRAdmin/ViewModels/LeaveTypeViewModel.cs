1a2b74f49f8592ec71c41313c06b61c4dc4d9538
Overrideveloper <bansowisdom@gmail.com>
2017-08-08 23:19:36 +0100

    Deleted LeaveType enum and added Location and LeaveType ViewModels

---
+++ BizzDesk-Leap-Client/Areas/HRAdmin/ViewModels/LeaveTypeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using BizzDesk_Leap_Client.Areas.HRAdmin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BizzDesk_Leap_Client.Areas.HRAdmin.ViewModels
+{
+    public class LeaveTypeViewModel
+    {
+        public LeaveType LeaveType
+        { 
+            get;
+            set; 
+        }
+    }
+}
---
+++ BizzDesk-Leap-Client/Areas/HRAdmin/ViewModels/LocationViewModel.cs
@@ -0,0 +1,17 @@
+﻿using BizzDesk_Leap_Client.Areas.HRAdmin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BizzDesk_Leap_Client.Areas.HRAdmin.ViewModels
+{
+    public class LocationViewModel
+    {
+        public Location Location
+        {
+            get;
+            set;
+        }
+    }
+}
